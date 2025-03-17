0c532d12c3d8b7311c7ccd29ceeff0ddff57d0a0
Untold-Ashu <ashu.goswami888@gmail.com>
2025-03-17 11:54:09 +0530

    socket url added

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("http://localhost:5000", {
+			const socket = io("https://quickchat-4urq.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
